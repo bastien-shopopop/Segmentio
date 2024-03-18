23ff0c6be62d153b6f5018edabf4cbad94229188
bastien <bastien.nizard@shopopop.com>
2024-03-18 17:13:04 +0100

    update badgeView

--- Segmentio/Source/Badge/Views/BadgeView.swift
+++ Segmentio/Source/Badge/Views/BadgeView.swift
@@ -11,6 +11,18 @@ class BadgeView: UIView {
     
     @IBOutlet fileprivate weak var contentView: UIView!
     
+    class func instanceFromNib() -> BadgeView {
+        let nibName = "BadgeView"
+        let podBundle = Bundle(for: self.classForCoder())
+        
+        if let bundleURL = podBundle.url(forResource: "Segmentio", withExtension: "bundle"), let bundle = Bundle(url: bundleURL) {
+            let badgeView = UINib(nibName: nibName, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as! BadgeView
+            badgeView.frame = CGRect(x: 0, y: 0, width: 7, height: 7) // Set frame size
+            return badgeView
+        }
+        return BadgeView(frame: CGRect(x: 0, y: 0, width: 7, height: 7))
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         initState()
@@ -30,17 +42,5 @@ class BadgeView: UIView {
     
     func setBadgeBackgroundColor(_ color: UIColor) {
         self.contentView.backgroundColor = color
-    }
-    
-    class func instanceFromNib() -> BadgeView {
-        let nibName = "BadgeView"
-        let podBundle = Bundle(for: self.classForCoder())
-        
-        if let bundleURL = podBundle.url(forResource: "Segmentio", withExtension: "bundle"), let bundle = Bundle(url: bundleURL) {
-                let badgeView = UINib(nibName: nibName, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as! BadgeView
-                badgeView.frame = CGRect(x: 0, y: 0, width: 7, height: 7) // Set frame size
-                return badgeView
-        }
-        return BadgeView(frame: CGRect(x: 0, y: 0, width: 7, height: 7))
     }
 }
